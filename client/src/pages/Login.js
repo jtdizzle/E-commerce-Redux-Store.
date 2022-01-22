aad714f7c7e1a0b73d81353ee9eca3d1d743f7cf
jonathan taylor <87590712+jtdizzle@users.noreply.github.com>
2022-01-21 23:02:24 -0500

    login

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,23 +1,22 @@
 import React, { useState } from 'react';
 import { useMutation } from '@apollo/client';
-import { Link } from 'react-router-dom';
 import { LOGIN } from '../utils/mutations';
 import Auth from '../utils/auth';
+import { Link } from 'react-router-dom';
+
 
 function Login(props) {
-  const [formState, setFormState] = useState({ email: '', password: '' });
+  const [formState, setFormState] = useState({ password: '', email: '' });
   const [login, { error }] = useMutation(LOGIN);
 
-  const handleFormSubmit = async (event) => {
+  const handleFormSubmit = async event => {
     event.preventDefault();
     try {
-      const mutationResponse = await login({
-        variables: { email: formState.email, password: formState.password },
-      });
+      const mutationResponse = await login({ variables: {password: formState.password,  email: formState.email  } })
       const token = mutationResponse.data.login.token;
       Auth.login(token);
     } catch (e) {
-      console.log(e);
+      console.log(e)
     }
   };
 
