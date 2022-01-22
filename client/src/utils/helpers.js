f98e2eac4eda206885edc97ee36a5517a2e5e5a4
jonathan taylor <87590712+jtdizzle@users.noreply.github.com>
2022-01-21 22:16:55 -0500

    helpers

--- client/src/utils/helpers.js
+++ client/src/utils/helpers.js
@@ -8,7 +8,9 @@ export function pluralize(name, count) {
 export function idbPromise(storeName, method, object) {
   return new Promise((resolve, reject) => {
     const request = window.indexedDB.open('shop-shop', 1);
+
     let db, tx, store;
+    
     request.onupgradeneeded = function(e) {
       const db = request.result;
       db.createObjectStore('products', { keyPath: '_id' });
